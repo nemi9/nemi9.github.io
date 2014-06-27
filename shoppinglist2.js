d05bdf71a7e015fc9301fdeaccd026a0b34ae390
Jason Turner <yourstageyourmusic@gmail.com>
2014-06-27 16:02:55 -0400

    Shopping List
    Shopping List for school

--- shoppinglist2.js
+++ shoppinglist2.js
@@ -1,65 +1,64 @@
-
 $(document).ready(function() {
-     
-     alert(0);   
+        
      
 $(".btn").click(function (e) {
-   e.preventDefault();
-   var text = $("input[name='add1']").val();
-   var $li = $("<li class='ui-state-default'/>").text(text);
-	  $("#names").append($li);
-	  $("#names").sortable('refresh');
+	      e.preventDefault();
+	      var text = $("input[name='add1']").val();
+	      var $li = $("<li class='ui-state-default'/>").text(text);
+        $("#names").append($li);
+        $("#names").sortable('refresh');
 });
      
      
-     
  $("#names").sortable({
-	containment: 'document', 
-	tolerance: 'pointer',
-	cursor: 'pointer', 
-	revert: true, 
-	opacity: 0.60,
-	connectWith:"#sale,#home,#names",
-	update: function(event, ui) {
-	   content = $(this).text();
-	   $('#sort_status').text(content);
-	
-	}
-})
-         
-   
-
-
+            containment: 'document', 
+            tolerance: 'pointer',
+            cursor: 'pointer', 
+            revert: true, 
+            opacity: 0.60,
+            connectWith:"#purchased,#home,#names",
+            update: function(event, ui) {
+            content = $(this).text();
+               
+    }
+});
+ 
+ 
+     
  $("#home").sortable({
-	containment: 'document', 
-	tolerance: 'pointer',
-	cursor: 'pointer', 
-	revert: true, 
-	opacity: 0.60,
-	connectWith:"#names,#home,#sale",
-	update: function(event, ui) {
-	   content = $(this).text();
-	   $('#sort_status').text(content); 
-		
-			 
-	}
-})
+            containment: 'document', 
+            tolerance: 'pointer',
+            cursor: 'pointer', 
+            revert: true, 
+            opacity: 0.60,
+            connectWith:"#names,#home,#purchased",
+            update: function(event, ui) {
+            content = $(this).text();
+             
+                
+                     
+   }
+});
 
 
-    
-$("#sale").sortable({
-	containment: 'document', 
-	tolerance: 'pointer',
-	cursor: 'pointer', 
-	revert: true, 
-	opacity: 0.60,
-	connectWith:"#sale,#names,#home",
-	update: function(event, ui) {
-		content = $(this).text();
-		$('#sort_status').text(content);
 
 
-       }
-    })
+$("#purchased").sortable({
+            containment: 'document', 
+            tolerance: 'pointer',
+            cursor: 'pointer', 
+            revert: true, 
+            opacity: 0.60,
+            connectWith:"#purchased,#names,#home",
+            update: function(event, ui) {
+            content = $(this).text();
+    $('#sort_status').text(content);
+
+
+      }
+     
+  });
+
+
 });
  
