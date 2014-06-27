a2f9968bf4397af97364a332f096fc61ef06bcad
Jason Turner <yourstageyourmusic@gmail.com>
2014-06-27 07:51:16 -0400

    Shopping List
    Shopping List for school

--- various_lesson.js
+++ various_lesson.js
@@ -1,18 +1,63 @@
-$(document).ready(function() {
-	
-	
 
-$(".shopping_list").click(function() {
-$("#names, #places").sortable({containment: 'parent', tolerance: 'pointer',
-   cursor: 'pointer', revert: true, opacity: 0.60, connectWith:"#names, #places"}) 
+  $(document).ready(function() {
+        
      
-	 
-	     update: function() {
-	     content = $(this).text()
-      $('#sort_status').text(content);
-}
-
+     $(".btn").click(function (e) {
+    e.preventDefault();
+    var text = $("input[name='add1']").val();
+    var $li = $("<li class='ui-state-default'/>").text(text);
+    $("#names").append($li);
+    $("#names").sortable('refresh');
 });
+     
+     
+     $("#names").sortable({
+            containment: 'document', 
+            tolerance: 'pointer',
+            cursor: 'pointer', 
+            revert: true, 
+            opacity: 0.60,
+         connectWith:"#places,#names",
+            update: function(event, ui) {
+                   content = $(this).text();
+                   $('#sort_status').text(content);
+            }
+        })
+        $("#places").sortable({
+            containment: 'document', 
+            tolerance: 'pointer',
+            cursor: 'pointer', 
+            revert: true, 
+            opacity: 0.60,
+            connectWith:"#names,#places",
+            update: function(event, ui) {
+                     content = $(this).text();
+                     $('#sort_status').text(content); 
+                
+                     
+            }
+        })
+
+
+
+
+         $("#sale").sortable({
+            containment: 'document', 
+            tolerance: 'pointer',
+            cursor: 'pointer', 
+            revert: true, 
+            opacity: 0.60,
+            connectWith:"#sale,#names,#places",
+            update: function(event, ui) {
+                     content = $(this).text();
+                     $('#sort_status').text(content);
+
+
+     }
+        })
+});
+ 
+
 
 
   
@@ -150,4 +195,3 @@ $(document).ready(function() {
 	
 
 
-});
