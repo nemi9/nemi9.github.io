9cae5b9eb9d3e18d35cf4f580b78d931b293e608
Jason Turner <yourstageyourmusic@gmail.com>
2014-06-29 14:28:28 -0400

    Shopping List
    Shopping List for school

---
+++ shopping_main.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function() {
+    $(".btn").click(function (e) {
+        e.preventDefault();
+        var text = $("input[name='add1']").val();
+        var $li = $("<li class='ui-state-default'/>").text(text);
+        $("#names").append($li);
+        $("#names").sortable('refresh');
+    });
+
+
+
+    $("#names").sortable({
+        containment: 'document', 
+        tolerance: 'pointer',
+        cursor: 'pointer', 
+        revert: true, 
+        opacity: 0.60,
+        connectWith:"#purchased,#home,#names",
+        update: function(event, ui) {
+            content = $(this).text();
+            
+        }
+     }); 
+     $("#home").sortable({
+        containment: 'document', 
+        tolerance: 'pointer',
+        cursor: 'pointer', 
+        revert: true, 
+        opacity: 0.60,
+        connectWith:"#names,#home,#purchased",
+        update: function(event, ui) {
+            content = $(this).text();
+             
+        }
+     });
+
+
+      
+
+
+
+
+     $("#purchased").sortable({
+         containment: 'document', 
+         tolerance: 'pointer',
+         cursor: 'pointer', 
+         revert: true, 
+         opacity: 0.60,
+         connectWith:"#purchased,#names,#home",
+         update: function(event, ui) {
+         content = $(this).text();
+         $('#sort_status').text(content);
+         
+
+          }
+     
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
